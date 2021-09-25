bfe420f84d0038f73f3a0c16ac7af68edc33ab00
Thrimal <thrimalavishka99@gmail.com>
2021-09-25 10:14:41 +0530

    first commit

---
+++ src/Customer.java
@@ -0,0 +1,15 @@
+/*
+ *
+ *  *---------------------------------------------------------------------------------------------
+ *  *  *  Copyright (c) GDSE-Gangsters. All rights reserved.
+ *  *  *  Licensed under the SriLankan Information License. See License.txt in the project root for license information.
+ *  *  *--------------------------------------------------------------------------------------------
+ *
+ */
+
+/**
+ * @author Thrimal Avishka <thrimalavishka99@gmail.com>
+ * @since 18-Sep-21
+ */
+public class Customer {
+}
---
+++ src/CustomerController.js
@@ -0,0 +1,4 @@
+/**
+ * @author Thrimal Avishka <thrimalavishka99@gmail.com>
+ * @since 18-Sep-21
+ */
